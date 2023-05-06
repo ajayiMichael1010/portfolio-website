89a80b0282b33d4431876ce559fb4ffa28db4054
AJAYI MICHAEL <MICHAEL@MACBOOKs-MacBook-Pro.local>
2023-05-06 11:14:05 +0100

    User controller updated

--- src/main/java/com/app/portfoliowebsite/controllers/UserController.java
+++ src/main/java/com/app/portfoliowebsite/controllers/UserController.java
@@ -1,4 +1,15 @@
 package com.app.portfoliowebsite.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "Login here";
+    }
 }
